e162e300f75a6e9473ccaaded347d34eb1cd7a8f
Kong Zheng <kongzheng1993@hotmail.com>
2019-10-23 10:38:30 +0800

    develop update

--- src/main/java/com/cmos/bj/ngtask/config/ScheduleConfig.java
+++ src/main/java/com/cmos/bj/ngtask/config/ScheduleConfig.java
@@ -7,13 +7,17 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.SchedulingException;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.config.ScheduledTask;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+import org.springframework.scheduling.config.TriggerTask;
 import org.springframework.scheduling.support.CronTrigger;
 import org.springframework.util.Assert;
 
 import java.util.Set;
+import java.util.concurrent.ScheduledFuture;
 
 /**
  * @Description:
@@ -71,6 +75,18 @@ public class ScheduleConfig implements SchedulingConfigurer {
         return scheduledTasks;
     }
 
+    /**
+     * 添加任务
+     */
+    public void addTriggerTask(String taskId, TriggerTask triggerTask) {
+        if (scheduledTasks.contains(taskId)) {
+            throw new SchedulingException("the taskId[" + taskId + "] was added.");
+        }
+
+        TaskScheduler scheduler = scheduledTaskRegistrar.getScheduler();
+        ScheduledFuture<?> future = scheduler.schedule(triggerTask.getRunnable(), triggerTask.getTrigger());
+
+    }
     
 
 
