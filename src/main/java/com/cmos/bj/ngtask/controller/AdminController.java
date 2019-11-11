c89d1bf8eb1e47a888b8d4de8a7f60977cfe5780
Kong Zheng <kongzheng1993@hotmail.com>
2019-11-11 17:55:11 +0800

    add 增加admin controller

--- src/main/java/com/cmos/bj/ngtask/controller/AdminController.java
+++ src/main/java/com/cmos/bj/ngtask/controller/AdminController.java
@@ -3,9 +3,15 @@ package com.cmos.bj.ngtask.controller;
 import com.cmos.bj.ngtask.entity.Admin;
 import com.cmos.bj.ngtask.repository.AdminRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
+/**
+ * @Author: kongz
+ */
 
 @RestController
 public class AdminController {
@@ -13,6 +19,7 @@ public class AdminController {
     @Autowired
     private AdminRepository adminRepository;
 
+    @GetMapping("/admin")
     public List<Admin> getAllAdmin() {
         return adminRepository.findAll();
     }
