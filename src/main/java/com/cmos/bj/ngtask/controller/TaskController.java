b3fb54de56842fb19c1e75ecea51ecc083bb3372
Kong Zheng <kongzheng1993@hotmail.com>
2019-11-13 18:22:23 +0800

    add 完成admin ，引入bootstrap-table

--- src/main/java/com/cmos/bj/ngtask/controller/TaskController.java
+++ src/main/java/com/cmos/bj/ngtask/controller/TaskController.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * @Description:
@@ -19,7 +19,7 @@ public class TaskController {
     @Autowired
     private TaskRepository taskRepository;
 
-    @RequestMapping("/task")
+    @GetMapping("/task")
     public String getAllTask(Model model, Pageable pageable) {
         model.addAttribute("task", taskRepository.findAll(pageable));
         return "task";
